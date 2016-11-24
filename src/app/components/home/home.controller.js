c31323f86684b00f6a6c2b6607b1db7bba49babf
Nusix <zdenokrnac@gmail.com>
2016-11-24 19:31:18 +0100

    Added for different block for homepage + Timeout added for counting

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -3,20 +3,94 @@
     1 - validation name ng pattern (numbers, letters, spaces, max 40 chars)
     2 - progress bars
     3 - error messages zdruzene
+    4 - countdown - aby sa nezacalo hned hrat ako potvrdi
+    5 - nejako lepsie pouzit timeout
 
     xy - pozriet, ako ozaj sa ma kodit callbacky a tak
 
 */
 
 export default class HomeController {
-    constructor($scope, $http, $interval, ScoreService, WordsService) {
+    constructor(ScoreService, WordsService, $timeout) {
         'ngInject';
 
-        this.userName = null;
+        // this.userName = null;
+        this.userName = 'asd';
         this.listOfWords = [];
-
         this.init(WordsService);
-    }
+        this.view = ['name-form','confirm-game','game','results'];
+        this.actualView = this.view[2];  //1-insert-name, 2-confirm game and rules 3-game 4-results
+        this.counter = 0;
+        this.timer;
+        // this.limit  = 40000;
+        this.limit = 3;
+
+        var self = this;
+
+        //timing
+        var stopCounter = function() {
+            $timeout.cancel(self.timer);
+            self.timer = null;
+
+            //TODO co sa stane
+        };
+
+        var startCounter = function() {
+            if (self.timer === null) {
+                updateCounter();
+            }
+        };
+
+        var updateCounter = function() {
+            if(self.counter < (self.limit-1)){
+                self.counter++;
+                self.timer = $timeout(updateCounter, 1000);
+                console.info('XXX volalo sa updateCounter cas', self.counter);
+            }else{
+                stopCounter();
+                console.info('XXX koniecvolalo sa updateCounter cas', self.counter);
+            }
+        };
+
+        updateCounter();
+    };
+
+    resetGame(){
+        this.userName = null;
+        this.counter = 0;
+        this.timer = null;
+        //TODO nacitat novy nahodny zoznam pismen
+    };
+
+    setView(view){
+        this.actualView = view;
+    };
+
+    cancelTheGame(){
+        this.resetGame();
+        this.setView(this.view[0]);
+    };
+
+    confirmTheGame(){
+        this.setView(this.view[2]);
+        // this.updateCounter(this.timeout);
+    };
+
+    // stopCounter() {
+    //     this.timeout.cancel(this.timer);
+    //     this.timer = null;
+    // };
+
+    // startCounter(){
+    //     if (this.timer === null) {
+    //         this.updateCounter();
+    //     }
+    // };
+
+    // updateCounter(){
+    //     this.counter++;
+    //     this.timer = this.timeout(this.updateCounter, 1000);
+    // };
 
     init(WordsService){
         var self = this;
@@ -30,6 +104,7 @@ export default class HomeController {
     }
 
     startTheGame(){
+        this.setView(this.view[1]);
         console.log('HomeController -> startTheGame : The game was started');
     };
 }
