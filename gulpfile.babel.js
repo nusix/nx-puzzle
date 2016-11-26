ccb54a8fda1e8509ddccb6221fac52402df4f210
Nusix <zdenokrnac@gmail.com>
2016-11-26 19:06:06 +0100

    package.json npm script changed for deploying

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,6 @@
 import gulp from 'gulp';
 import shell from 'gulp-shell';
 import clean from 'gulp-clean';
-import deploy from 'gulp-gh-pages';
-
 
 gulp.task('serve', shell.task([
   'webpack-dev-server --config ./config/webpack.dev.babel.js --colors --progress'
@@ -18,16 +16,7 @@ gulp.task('test:run', shell.task([
   'karma start ./config/karma/karma.conf.js --colors --single-run --fail-on-hint'
 ]));
 
-
 gulp.task('test', gulp.task('test:run'));
-
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
-});
 
 
 gulp.task('clean', () => gulp.src(['reports', 'node_modules'], { base: './', read: false}).pipe(clean()));
